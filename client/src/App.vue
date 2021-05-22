e53cf668c7b7ebeaa483ffd7bd8c99c15936d3bd
Susan Ashmore <scashmorecitcom@gmail.com>
2021-05-22 01:41:48 -0400

    basic structure

---
+++ client/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="app">
+      <Game1 />
+    </div>
+</template>
+
+<script>
+    import Game1 from "./components/Game1.vue";
+
+    export default {
+      name: "App",
+      components: {
+        Game1,
+      },
+    };
+</script>
+
+<style>
+</style>
